29ecf2211fc626e05a51d91c1315e43a01e86e6f
imane omari <imane3.omari@gmail.com>
2023-08-12 01:32:13 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    dodger.style.left = parseInt(dodger.style.left) - 1 + 'px';
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    dodger.style.left = parseInt(dodger.style.left) + 1 + 'px';
+  }
+  
+  
